0328c92fafd0e900f9d60bcfe04ef2dcd21d1392
Pablo Pacheco <pablopdomingos@gmail.com>
2017-01-10 01:57:09 -0200

    Adiciona serviço de consulta de NFSe

---
+++ nfse-bh/src/main/java/com/pablodomingos/classes/rps/NFSeConsulta.java
@@ -0,0 +1,67 @@
+package com.pablodomingos.classes.rps;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+
+@XStreamAlias("ConsultarNfseEnvio")
+public class NFSeConsulta {
+
+  @XStreamAlias("xmlns")
+  @XStreamAsAttribute
+  private String xmlns = "http://www.abrasf.org.br/nfse.xsd";
+
+  @XStreamAlias("Prestador")
+  @NotNull
+  private RpsPrestador prestador;
+
+  @XStreamAlias("NumeroNfse")
+  @Size(min = 15, max = 15)
+  private String numeroNFSe;
+  
+  @XStreamAlias("PeriodoEmissao")
+  @NotNull
+  private PeriodoEmissao periodoEmissao;
+  
+  @XStreamAlias("Tomador")
+  private RpsTomadorIdentificacao tomador;
+
+  @XStreamAlias("IntermediarioServico")
+  private RpsIntermediario intermediario;
+  
+  public NFSeConsulta(RpsPrestador prestador, LocalDate dataInicial, LocalDate dataFinal) {
+    super();
+    this.prestador = prestador;
+    this.periodoEmissao = new PeriodoEmissao(dataInicial, dataFinal);
+  }
+
+  public NFSeConsulta comTomador(RpsTomadorIdentificacao tomador){
+    this.tomador = tomador;
+    return this;
+  }
+  
+  public NFSeConsulta comIntermediario(RpsIntermediario intermediario){
+    this.intermediario = intermediario;
+    return this;
+  }
+  
+  public RpsPrestador getPrestador() {
+    return prestador;
+  }
+
+  public String converterParaXml() {
+    String XML_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+    
+    XStream xstream = new XStream();
+    xstream.setMode(XStream.NO_REFERENCES);
+    xstream.autodetectAnnotations(true);
+    
+    return XML_HEADER + xstream.toXML(this);
+  }
+
+}
---
+++ nfse-bh/src/main/java/com/pablodomingos/classes/rps/PeriodoEmissao.java
@@ -0,0 +1,39 @@
+package com.pablodomingos.classes.rps;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import com.pablodomingos.conversores.LocalDateConversor;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+
+
+public class PeriodoEmissao {
+  @XStreamAlias("DataInicial")
+  @XStreamConverter(LocalDateConversor.class)
+  @NotNull
+  private LocalDate dataInicial;
+
+  @XStreamAlias("DataFinal")
+  @XStreamConverter(LocalDateConversor.class)
+  @NotNull
+  private LocalDate dataFinal;
+
+  public PeriodoEmissao(LocalDate dataInicial, LocalDate dataFinal) {
+    this.dataInicial = dataInicial;
+    if (dataFinal.isAfter(LocalDate.now())) {
+      throw new DateTimeException("Data final nao pode ser maior que a atual");
+    }
+    this.dataFinal = dataFinal;
+  }
+
+  public LocalDate getDataInicial() {
+    return dataInicial;
+  }
+
+  public LocalDate getDataFinal() {
+    return dataFinal;
+  }
+}
