119cd04d7ba115404e3b1cc462b218f0913b2416
Pablo Pacheco <pablopdomingos@gmail.com>
2017-01-12 20:49:24 -0200

    Testes de Intermediario e Prestador

--- nfse-bh/src/test/java/com/pablodomingos/classes/rps/RpsPrestadorTest.java
+++ nfse-bh/src/test/java/com/pablodomingos/classes/rps/RpsPrestadorTest.java
@@ -9,15 +9,6 @@ import com.pablodomingos.classes.rps.builders.PrestadorBuilder;
 public class RpsPrestadorTest {
 
   @Test(expected = ConstraintViolationException.class)
-  public void naoDevePermitirCnpjTamanhoInvalido() throws Exception {
-    try {
-      new PrestadorBuilder("").build();
-    } catch (final ConstraintViolationException e) {
-      new PrestadorBuilder("123456789012300").build();
-    }
-  }
-
-  @Test(expected = ConstraintViolationException.class)
   public void naoDevePermitirCnpjInvalido() throws Exception {
     new PrestadorBuilder("12345678901231").build();
   }
